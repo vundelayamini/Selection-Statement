c64c97893c259af8d5e846d69df2784ced9f96d3
yamini <vundelayamini@gmail.com>
2021-02-18 10:59:06 +0530

    Added : example for displayNumCase

--- displaynumIfelse.sh
+++ displaynumIfelse.sh
@@ -5,22 +5,22 @@ echo $num
 if (( $num >= 1 ))
    then
        echo"unit"
-elif [ $num >= 10 ]
+elif (( $num >= 10 ))
     then
      echo "ten"
-elif [ $num >= 100 ]
+elif (( $num >= 100 ))
     then
          echo" hundred"
-elif [ $num >= 1000 ]
+elif (( $num >= 1000 ))
      then
            echo"thousand"
-elif [ $num >= 10000 ]
+elif (( $num >= 10000 ))
       then
            echo" ten thousand"
-elif [ $num >= 100000 ]
+elif (( $num >= 100000 ))
       then
           echo "one laksh"
-elif [ $num >= 1000000 ]
+elif (( $num >= 1000000 ))
        then
             echo"ten laksh"
 else
